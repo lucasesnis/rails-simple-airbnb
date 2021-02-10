c27c33cf7859a8162d4398096644181129f7c59d
Lucas Esnis <esnis.lucas@gmail.com>
2021-02-10 15:29:31 +0100

    a

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Superbe vue montmartre',
+  address: 'Colline Montmartre',
+  description: ' balcon de ouf ',
+  price_per_night: 100,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: "Centre d'Issy",
+  address: 'Mairie',
+  description: 'Metro à deux min',
+  price_per_night: 55,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Du sale au mic',
+  address: 'MLK',
+  description: 'Devenir Malakofiot',
+  price_per_night: 10,
+  number_of_guests: 8
+)
